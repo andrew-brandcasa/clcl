d694aa36d65cac57aa87030d594a56ed5eb047fe
andrew-brandcasa <andrew@brandcasa.io>
2025-09-18 12:16:37 -0400

    Fix tailwind.config.js syntax error

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,14 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     container: { center: true, padding: { DEFAULT: "1rem", lg: "2rem" } },
+    extend: {
       maxWidth: { content: "72rem" }, /* ~1152px */
       fontFamily: {
         serif: ['"Libre Baskerville"', "serif"],
         sans: ['"Nunito Sans"', "system-ui", "sans-serif"]
       },
       letterSpacing: { tight2: "-0.02em" }
-    extend: {},
+    },
   },
   plugins: [],
 };
