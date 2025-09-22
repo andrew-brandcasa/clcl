c5061bd40b654245e1c09c95e2b353fd4a791dba
andrew-brandcasa <andrew@brandcasa.io>
2025-09-22 15:57:15 -0400

    Updated package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Welcome from './components/Welcome';
 import OurWines from './components/OurWines';
@@ -8,49 +7,22 @@ import OurVenue from './components/OurVenue';
 import WineOnWheels from './components/WineOnWheels';
 import UpcomingEvents from './components/UpcomingEvents';
 import Footer from './components/Footer';
-import About from './pages/About';
-import PrivateEvents from './pages/PrivateEvents';
-import Header from './components/Header';
 import './App.css';
-
-const HomePage = () => (
-  <>
-    <Hero />
-    <Welcome />
-    <OurWines />
-    <OurHistory />
-    <OurVenue />
-    <WineOnWheels />
-    <UpcomingEvents />
-  </>
-);
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={
-            <>
-              <HomePage />
-            </>
-          } />
-          <Route path="/about" element={
-            <>
-              <Header />
-              <About />
-            </>
-          } />
-          <Route path="/private-events" element={
-            <>
-              <Header />
-              <PrivateEvents />
-            </>
-          } />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <main>
+        <Hero />
+        <Welcome />
+        <OurWines />
+        <OurHistory />
+        <OurVenue />
+        <WineOnWheels />
+        <UpcomingEvents />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
